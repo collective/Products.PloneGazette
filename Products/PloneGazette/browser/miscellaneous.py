10ed3e447fa3245edafd8ac6ea5f230e146ca98b
Taito Horiuchi <taito.horiuchi@hexagonit.fi>
2012-05-24 16:39:42 +0300

    Unsubscribe added to actions.

--- Products/PloneGazette/browser/miscellaneous.py
+++ Products/PloneGazette/browser/miscellaneous.py
@@ -1,4 +1,5 @@
 from Acquisition import aq_inner
+from Acquisition import aq_parent
 from Products.CMFCore.utils import getToolByName
 from Products.Five.browser import BrowserView
 from Products.PythonScripts.standard import url_quote
@@ -21,3 +22,13 @@ class Miscellaneous(BrowserView):
                    if action['id'] == 'subscribe'][0]
             query_url = '%s?email=%s&format=%s' % (url, url_quote(email), format)
             self.request.response.redirect(query_url)
+
+    def unsubscribe(self):
+        """Unsubscribe the subscriber."""
+        context = aq_inner(self.context)
+        oid = context.id
+        newsletters = aq_parent(context)
+        newsletters.unSubscribe(
+            oid,
+            REQUEST=self.request
+        )
