f9744cfa45dd2d577b4e6be2552bfe6c07359b70
Taito Horiuchi <taito.horiuchi@hexagonit.fi>
2012-05-25 12:43:46 +0300

    Upgrade step to import factorytool.xml added.

--- Products/PloneGazette/migration.py
+++ Products/PloneGazette/migration.py
@@ -5,6 +5,9 @@ import logging
 logger = logging.getLogger('PloneGazette.migration')
 
 
+PROFILE_ID = 'profile-Products.PloneGazette:default'
+
+
 def _remapSubscriberWorkflow(context):
     type_ids = ('Subscriber',)
     chain = ('subscriber_workflow',)
@@ -13,6 +16,7 @@ def _remapSubscriberWorkflow(context):
     remap_workflow(context, type_ids=type_ids, chain=chain,
                    state_map=state_map)
     logger.info('Subscriber workflow remapped')
+
 
 def _ensureSubscriberFoldersPrivate(context):
     portal = context.portal_url.getPortalObject()
@@ -28,5 +32,19 @@ def _ensureSubscriberFoldersPrivate(context):
 def migrateTo311(context):
     _remapSubscriberWorkflow(context)
 
+
 def migrateTo32(context):
     _ensureSubscriberFoldersPrivate(context)
+
+
+def upgrade_32_to_33(context):
+    """Import factorytool.xml"""
+    setup = getToolByName(context, 'portal_setup')
+    logger.info('Importing factorytool.xml.')
+    setup.runImportStepFromProfile(
+        PROFILE_ID,
+        'factorytool',
+        run_dependencies=False,
+        purge_old=False
+    )
+    logger.info('Imported factorytool.xml')
