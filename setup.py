da7abf20d9c55ad753241214afb9a300f66585ea
Martin Peeters <martin.peeters@affinitic.be>
2014-02-25 11:26:58 +0100

    Back to development: 3.2.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = (
 
 setup(
     name='Products.PloneGazette',
-    version='3.2.0.1',
+    version='3.2.1.dev0',
     author='Pilot Systems, Nidelven IT LTD and others',
     author_email='',
     maintainer='Morten W. Petersen',
